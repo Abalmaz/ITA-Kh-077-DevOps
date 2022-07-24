b934f8d3e7a69718cb69d9508ba6e1aaf901f455
Yana Abalmaz <yana.abalmaz@gmail.com>
2022-07-24 14:22:44 +0300

    Added answers to bash task

--- Bash/script_c.sh
+++ Bash/script_c.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+COMMAND_LINE_OPTIONS_HELP='
+The data backup script that takes the following data as parameters:
+- path to the syncing directory
+- the path to the directory where the copies of the files will be stored
+Usage: '`basename $0`'  [syncing_dir] [dir_to_backup]
+
+Examples:
+        '`basename $0`' /home/username/db  /home/username/backups
+'
+
+function info {
+  echo "$COMMAND_LINE_OPTIONS_HELP"
+  exit 1;
+}
+
 function checkDirectory(){
   if [ "$1" = "" ]; then
     echo "You didn't give the the syncing directory. "
-    exit 1
+    info
 elif [ ! -d "$1" ]; then
     echo "The directory $1 does not exist."
-    exit 1
+    info
 fi
 }
-
-readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
-readonly BACKUP_PATH="$1/${DATETIME}"
 
 checkDirectory $1
 checkDirectory $2
 
-rsync -av --delete "$1/"  --exclude=".cache" "${BACKUP_PATH}"
+rsync -av --delete --out-format "%t %o %n" --log-file=/var/log/rsync/srync.log "$1/" "$2"
+
+
+# For adding script to cron for run every minute:
+# crontab -e
+# or
+# sudo nano /etc/crontab
+# * * * * * /path_to_script/script_c.sh [syncing_dir] [dir_to_backup]
